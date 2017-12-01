d806cfbf07ea6c67effa1f52c5f80033e3642783
mjvakili <mjvakili@nyu.edu>
2017-12-01 17:12:21 +0100

    updating cm fitting

--- code/gr_fitting.py
+++ code/gr_fitting.py
@@ -46,6 +46,110 @@ def filter(style):
 
     return a
 
+def sdss_reduction():
+    
+    gama = pf.open("data/KiDSxGAMAequ+G23.fits")[1].data
+    sdss = pf.open("data/KiDS.DR3.x.SDSS.DR13.galaxies.fits")[1].data
+    mask = np.where(np.in1d(sdss['ID'] , gama['ID'])==False)
+    sdss = sdss[mask]
+    data = sdss
+    data = data[data['IMAFLAGS_ISO_U']&01010111==0]
+    data = data[data['IMAFLAGS_ISO_G']&01010111==0]
+    data = data[data['IMAFLAGS_ISO_R']&01010111==0]
+    data = data[data['IMAFLAGS_ISO_I']&01010111==0]
+    data = data[data['MAGERR_GAAP_U']>0]
+    data = data[data['MAGERR_GAAP_G']>0]
+    data = data[data['MAGERR_GAAP_R']>0]
+    data = data[data['MAGERR_GAAP_I']>0]
+    u = data['MAG_GAAP_U'] + data['ZPT_OFFSET_U'] - data['EXT_SFD_U']
+    g = data['MAG_GAAP_G'] + data['ZPT_OFFSET_G'] - data['EXT_SFD_G']
+    r = data['MAG_GAAP_R'] + data['ZPT_OFFSET_R'] - data['EXT_SFD_R']
+    i = data['MAG_GAAP_I'] + data['ZPT_OFFSET_I'] - data['EXT_SFD_I']
+    mask = (u<25.4)&(g<25.6)&(r<24.7)&(i<24.5)
+    data = data[mask]
+    u = data['MAG_GAAP_U'] + data['ZPT_OFFSET_U'] - data['EXT_SFD_U']
+    g = data['MAG_GAAP_G'] + data['ZPT_OFFSET_G'] - data['EXT_SFD_G']
+    r = data['MAG_GAAP_R'] + data['ZPT_OFFSET_R'] - data['EXT_SFD_R']
+    i = data['MAG_GAAP_I'] + data['ZPT_OFFSET_I'] - data['EXT_SFD_I']
+    uerr = data['MAGERR_GAAP_U']
+    gerr = data['MAGERR_GAAP_G']
+    rerr = data['MAGERR_GAAP_R']
+    ierr = data['MAGERR_GAAP_I']
+    Z = data['Z']
+    RA = data['RA']
+    DEC = data['DEC']
+    u_g = data['COLOR_GAAPHOM_U_G']
+    g_r = data['COLOR_GAAPHOM_G_R']
+    r_i = data['COLOR_GAAPHOM_R_I']
+    col = np.vstack([u,g,r,i,uerr,gerr,rerr,ierr,u_g,g_r,r_i,Z,RA,DEC])
+
+    return col
+    
+def gama_reduction():
+
+
+    data = gama
+        
+    data = data[data['IMAFLAGS_ISO_U']&01010111==0]
+    data = data[data['IMAFLAGS_ISO_G']&01010111==0]
+    data = data[data['IMAFLAGS_ISO_R']&01010111==0]
+    data = data[data['IMAFLAGS_ISO_I']&01010111==0]			    
+    data = data[data['MAGERR_GAAP_U']>0]
+    data = data[data['MAGERR_GAAP_G']>0]
+    data = data[data['MAGERR_GAAP_R']>0]
+    data = data[data['MAGERR_GAAP_I']>0]
+    data = data[data['MAG_GAAP_u_CALIB']<25.4]
+    data = data[data['MAG_GAAP_g_CALIB']<25.6]
+    data = data[data['MAG_GAAP_r_CALIB']<24.7]
+    data = data[data['MAG_GAAP_i_CALIB']<24.5]
+    u = data['MAG_GAAP_u_CALIB']
+    g = data['MAG_GAAP_g_CALIB']									        
+    r = data['MAG_GAAP_r_CALIB']										    
+    i = data['MAG_GAAP_i_CALIB']
+    uerr = data['MAGERR_GAAP_U']
+    gerr = data['MAGERR_GAAP_G']
+    rerr = data['MAGERR_GAAP_R']
+    ierr = data['MAGERR_GAAP_I']
+
+    Z = data['Z']
+    RA = data['RA']
+    DEC = data['DEC']
+    u_g = data['COLOR_GAAPHOM_U_G']
+    g_r = data['COLOR_GAAPHOM_G_R']
+    r_i = data['COLOR_GAAPHOM_R_I']
+    col = np.vstack([u,g,r,i,uerr,gerr,rerr,ierr,u_g,g_r,r_i,Z,RA,DEC])													    
+    
+    return col 
+
+def catalog_combinator():
+    '''
+    combines sdss and gama catalogs 
+    '''
+    combined_cat = np.hstack([gama_reduction() , sdss_reduction()])
+
+    return combined_cat
+
+
+def catalog_slicer(zmin, zmax, component):
+
+    
+    combined_cat = catalog_combinator()
+    mask = (z>zmin) & (z<zmax)
+    reduced_cat = combined_cat[:,mask]
+    
+    x = reduced_cat[3,:] #mi the reference magnitude
+
+    color = reduced_cat[8:11,:]
+    color_err = np.zeros_like(color)
+    color_err[0,:] = reduced_cat[4,:]**2. + reduced_cat[5,:]**2.
+    color_err[1,:] = reduced_cat[5,:]**2. + reduced_cat[6,:]**2.
+    color_err[2,:] = reduced_cat[6,:]**2. + reduced_cat[7,:]**2.
+
+    y = color[component, :]
+    yerr = color[component , :]
+
+    return x, y, yerr
+
 def test(zmin , zmax , component):
     '''
     test returns x = mi 
