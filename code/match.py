4be3f462a36e7c40917b78f068e5e3c29dd8d804
mjvakili <mjvakili@nyu.edu>
2018-03-13 14:16:38 +0100

    matching 2df and kids south

---
+++ code/match.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pyfits as pf
+import h5py
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+
+
+def match_2df_kids():
+
+    '''
+    function fo matching the sky 
+    coordinate of 2dflens lrgs
+    to their kids counterparts
+    '''
+    ### KiDS ###
+    kids_cat = h5py.File("reduced_kids.h5" , 'r')
+    kids_ra = kids_cat["RA"]
+    kids_dec = kids_cat["DEC"]
+    kids_id = kids_cat["ID"]
+    
+    ###2dfLenS LoZ & HiZ###
+
+    df_cat_loz = np.loadtxt("data/data_loz_atlas_kidss_160105_ntar.dat")
+    df_cat_hiz = np.loadtxt("data/data_hiz_atlas_kidss_160105_ntar.dat")
+    loz_ra = df_cat_loz[:,0]
+    hiz_ra = df_cat_hiz[:,0]
+    loz_dec = df_cat_loz[:,1]
+    hiz_dec = df_cat_hiz[:,1]
+    loz_z = df_cat_loz[:,2]
+    hiz_z = df_cat_hiz[:,2]
+
+    df_z = np.hstack([loz_z, hiz_z])
+    df_ra = np.hstack([loz_ra, hiz_ra])
+    df_dec = np.hstack([loz_dec, hiz_dec])
+
+    c_kids = SkyCoord(ra=kids_ra*u.degree, dec=kids_dec*u.degree)   
+    c_df = SkyCoord(ra=df_ra*u.degree, dec=df_dec*u.degree)   
+
+    idx, d2d, d3d = c_df.match_to_catalog_sky(c_kids) 
+    
+    print "matching done"
+    
+    np.savetxt("2df_matched_kids.txt" , idx)
+
+    return None
+
+if __name__ == '__main__':
+
+    match_2df_kids()
+
+
+
