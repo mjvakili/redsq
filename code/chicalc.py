83b7bea6d132df1c233b120290a43281456ebe8a
mjvakili <mjvakili@nyu.edu>
2018-03-26 15:34:53 +0200

    chi calibation

---
+++ code/chicalc.py
@@ -0,0 +1,229 @@
+
+import numpy as np
+import h5py
+from scipy.interpolate import CubicSpline
+import ezgal
+import cosmolopy.distance as cd
+import util
+import kids_gama
+import emcee
+from mixture_filtering import catalog_combinator
+import pyfits as pf
+
+def dvdz(z):
+    '''
+    dv/dz to impose uniformity
+    in redshift
+    '''
+    cosmo = {'omega_M_0':0.3, 'omega_lambda_0':0.7, 'omega_k_0':0.0, 'h':0.72}
+    d_a = cd.angular_diameter_distance(z, **cosmo) #angular diameter distance
+    h = cd.e_z(z, **cosmo) #hubble parameter
+    dvdz = (1+z)**2. * d_a **2  * h **-1. #dv/dz
+    return dvdz
+
+def luminosity(m,z):
+    '''
+    L/Lstar of redsequence galaxies
+    '''
+    mchar = kids_mstar(z)
+    dm = m - mchar
+    exparg = 10. ** (-0.4 * dm)
+
+    return exparg
+    
+class calibrate(object):
+
+    def __init__(self, zmin, zmax, dz, Nb, Nm, Nf, Nhis, Nab, Nchi):
+       
+        # the inferred c-m relation parameters
+        self.theta = theta
+
+        # ID, color, mag of all gals
+        self.ID = ID
+        self.colors = colors
+        self.color_errs = color_errs
+        self.mis = mis
+        
+        # ID and z of calib gals
+        self.ID_calib = ID_calib
+        self.z_calib = z_calib
+       
+        #CROSS MATCH BETWEEN ALL and calib gals
+
+        self.mask_calib = np.where(np.in1d(self.ID_calib , self.ID)==True)
+
+        # initial estimates of chi, l, z
+        self.chi_init = chi_init
+        self.l_init = l_init
+        self.z_init = z_init
+
+        # sum numbers for making an initial cut
+        self.chi_abs = chi_abs
+        self.zmin_abs = zmin_abs
+        self.zmax_abs = zmax_abs
+        self. l_abs = l_abs
+
+
+        self.zmin = zmin #minimum redshift
+        self.zmax = zmax #maximum redshift
+        self.dz = dz     #width of redshift bins
+        self.Nb = Nb
+	self.Nm = Nm
+	self.Nf = Nf
+
+        
+        self.bnod = np.linspace(self.zmin,self.zmax,self.Nb) #spacing of .05
+        self.mnod = np.linspace(self.zmin,self.zmax,self.Nm) #spacing of .1
+        self.fnod = np.linspace(self.zmin,self.zmax,self.Nf) #spacing of .14
+        self.xrefnod = np.linspace(self.zmin,self.zmax,20) #spacing of .05 
+        self.bnod = .5*(self.bnod[1:]+self.bnod[:-1])
+        self.fnod = .5*(self.fnod[1:]+self.fnod[:-1])
+        self.mnod = .5*(self.mnod[1:]+self.mnod[:-1])
+
+
+        self.Nhis = Nhis  #the number of bins for making hist of data
+        self.Nab = Nab    #the number of spline nodes for afterburner
+        self.Nchi = Nchi  #the number of spline nodes for parameterizing chimax
+        self.hisnod = np.linspace(self.zmin,self.zmax,self.Nhis) #the nods at which we make hist of data 
+        self.abnod = np.linspace(self.zmin,self.zmax,self.Nab) # the afterburner nods
+        self.chinod = np.linspace(self.zmin,self.zmax,self.Nchi) #the chimax nods
+
+
+        self.theta = np.loadtxt("opt_theta_bfgs_bounded2.txt")
+        self.m = self.theta[0:3*(self.Nm-1)].reshape(self.Nm-1,3) #array of m-nodes
+        self.b = self.theta[3*(self.Nm-1):3*(self.Nm+self.Nb-2)].reshape(self.Nb-1,3) #array of b-nodes
+        self.lnf = self.theta[3*(self.Nm+self.Nb-2):].reshape(self.Nf-1,3) #array of lnf-nodes
+
+        ####################################HACKY ###############################
+        self.bnod = self.bnod[:-1]
+        self.mnod = self.mnod[:-1]
+        self.fnod = self.fnod[:-1]
+        self.m = self.m[:-1,:]
+        self.b = self.b[:-1,:]
+        self.lnf = self.lnf[:-1,:]
+        ####################################################################
+        red_file = h5py.File("red_cat.hdf5" , 'r')
+        red_sample = red_file['red'][:]
+        mrefs = red_file['mref'][:]
+        red_file.close()
+        znods = np.linspace(self.zmin, self.zmax, 36)
+	znods = .5*(znods[1:]+znods[:-1])
+	#####################################HACKY#############################
+        znods = znods[znods<0.7]
+        mrefs = mrefs[znods<0.7]
+	#######################################################################
+        self.xref = CubicSpline(znods, mrefs)(self.xrefnod)
+
+        return None
+
+    def interpolate(self , z):
+        """
+        interpolate a , b , c arrays from 
+        nodes to a given z
+        """
+
+        bz = CubicSpline(self.bnod , self.b)(z)
+        mz = CubicSpline(self.mnod , self.m)(z)
+        lnfz = CubicSpline(self.fnod , self.lnf)(z)
+        xrefz = CubicSpline(self.xrefnod , self.xref)(z)
+        
+        return mz, bz, lnfz, xrefz
+
+    def chi3d(self, z):
+        """
+        fast calculation of all the chis for a set of zs 
+        z = the current estimate of reshifts
+        """
+        bz = CubicSpline(self.bnod , self.b)(z)
+        mz = CubicSpline(self.mnod , self.m)(z)
+        lnfz = CubicSpline(self.fnod , self.lnf)(z)
+        xrefz = CubicSpline(self.xrefnod , self.xref)(z)
+
+        Cint = np.zeros((len(z), 3, 3))
+        Cint[:,0,0] = np.exp(2.* lnfz[:,0])
+        Cint[:,1,1] = np.exp(2.* lnfz[:,1])
+        Cint[:,2,2] = np.exp(2.* lnfz[:,2])
+        Ctot = self.color_errs + Cint
+        res = mz * (self.mis - xrefz)[:,None] + bz - self.colors
+        chis = np.einsum('mn,mn->n', res, np.einsum('ijk,ik->ij',np.linalg.inv(Ctot),res))
+    
+        return chis
+    
+    def l3d(self, z):
+        """
+        fast calculation of all the luminosity ratios for a set of zs 
+        z = the current estimate of reshifts
+        """
+        ls = luminosity(self.mis, z)
+                 
+        return ls
+
+         
+    def ab_lnlike(dz_theta, y, nods, z):
+        """
+        dz_theta = estimate of dz at ab spline nods
+        z = current estimated of zred
+
+        """
+        x , y = sample2[:,1], sample2[:,1] - redshift
+        
+        dz_pred = CubicSpline(self.abnod , dz_theta)(z)
+        chisq = np.sum(np.abs(y - dz_pred))
+    
+        return chisq
+
+if __name__ == '__main__':
+
+   ############ THE ENTIRE KIDS WITH COLORS AND MAGS #############
+   reduced_kids = h5py.File("reduced_kids.h5" , "r")
+
+   ID = reduced_kids['ID'][:34290650]
+   RA  = reduced_kids['RA'][:34290660]
+   DEC = reduced_kids['DEC'][:34290660]
+   DEC = reduced_kids['DEC'][:34290660]
+   mi = reduced_kids['mi'][:34290660]
+   redshift = reduced_kids['redshift'][:34290660]
+   colors = reduced_kids['colors'][:34290660]
+   color_errs = reduced_kids['color_errs'][:34290660]
+   
+   ########## THE ENTIRE KIDS WITH UNCALIB Z l CHI #############
+   red_sample = h5py.File("red_photometric_sample_v2.h5" , "r")
+   red_sample = sample["opt"][:34290660]
+
+   ######### KiDS GALAXIES WITH SPECZ FOR CALIBRATION ######## 
+   spec_kids = h5py.File("reduced_specallkids.h5" , 'r')
+   spec_ID = spec_kids["ID"][:]
+   spec_z = spec_kids["redshift"][:]
+   
+   ######## MATCHED SPECZ & REDZ ARRAY FOR CALIBRATION #######
+   
+   spec_ID , spec_index = np.unique(spec_kids["ID"][:] , return_index = True)
+   spec_z = spec_kids["redshift"][:][spec_index]
+   mask_one = np.where(np.in1d(ID,spec_ID)==True)[0]
+   mask_two = np.where((np.in1d(spec_ID,ID[mask_one])==True))[0]
+   #specz = spec_z[mask_two]
+   arg_one = np.argsort(ID[mask_one])
+   arg_two = np.argsort(spec_ID[mask_two])
+
+   print (ID[mask_one][arg_one] == spec_ID[mask_two][arg_two]) #sanity check!
+
+   spec_red_sample = red_sample[mask_one][arg_one]
+   mask_chil = (spec_red_sample[:,0]==1)
+   spec_specz = spec_z[mask_two][arg_two]
+   
+   calib_sample = np.vstack([spec_red_sample.T , spec_specz]).T
+   
+   print calib_sample
+   
+
+   ########## INITIALIZATION OF THE EZGAL MODEL ########
+
+   model = ezgal.model("/net/delft/data2/vakili/easy/ezgal_models/www.baryons.org/ezgal/models/bc03_burst_0.1_z_0.02_salp.model")
+   zf = 3.0 #HARDCODED
+   model.add_filter("/net/delft/data2/vakili/easy/i.dat" , "kids" , units = "nm")
+   #kcorr_sloan = model.get_kcorrects(zf=3.0 , zs = 0.25 , filters = "sloan_i")
+   model.set_normalization("sloan_i" , 0.2 , 17.85, vega=False, apparent=True)
+    
+   cosmo = {'omega_M_0':0.3, 'omega_lambda_0':0.7, 'omega_k_0':0.0, 'h':1.0}
+   zmin , zmax , dz = 0.1 , 0.8 , 0.02
+   sampler(0.1, 0.8, 0.02, 10, 1000, 2000, 60)
