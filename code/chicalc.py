2b7b8e870d4f9a71149a35275c65ef20c87e014d
mjvakili <mjvakili@nyu.edu>
2018-03-29 13:46:50 +0200

    fixed a few bugs in chimax calib

--- code/chicalc.py
+++ code/chicalc.py
@@ -6,17 +6,7 @@ import ezgal
 import cosmolopy.distance as cd
 import util
 import emcee
-
-def dvdz(z):
-    '''
-    dv/dz to impose uniformity
-    in redshift
-    '''
-    cosmo = {'omega_M_0':0.3, 'omega_lambda_0':0.7, 'omega_k_0':0.0, 'h':0.72}
-    d_a = cd.angular_diameter_distance(z, **cosmo) #angular diameter distance
-    h = cd.e_z(z, **cosmo) #hubble parameter
-    dvdz = (1+z)**2. * d_a **2  * h **-1. #dv/dz
-    return dvdz
+import scipy.optimize as op
 
 def vc(z):
     '''
@@ -37,16 +27,25 @@ def luminosity(m,z):
 
     return exparg
     
+def kids_mstar(zs):
+    '''
+    returns mstar of redsequence galaxies 
+    as observed by kids i band
+    '''
+    #kcorr = model.get_kcorrects(zf=3.0, zs=zs , filters = "kids") #WE SHALL KCORRECT THE MAGS IF NEEDED
+    mags = model.get_apparent_mags(zf=zf , filters = "kids" , zs= zs)
+
+    return mags
 class calibrate(object):
 
-    def __init__(self, zmin, zmax, dz, Nb, Nm, Nf, Nhis, Nab, Nchi):
+    def __init__(self, zmin, zmax, dz, Nb, Nm, Nf, 
+                 Nhist, Nab, Nchi , lmin, nbar, 
+		 z, l, chi, colors, color_errs, mis, 
+		 calib):
         
         # selection summary
 	self.lmin = lmin
         self.nbar = nbar
-
-        # the inferred c-m relation parameters
-        self.theta = theta
 
         # ID, color, mag of all gals
         self.ID = ID
@@ -63,15 +62,15 @@ class calibrate(object):
         self.calib = calib   # the whole calibration sample consisting of ID, zrm, chi, l, zspec. the same as calib_sample defined below
 
         # initial estimates of chi, l, z
-        self.chi_init = chi_init
-        self.l_init = l_init
-        self.z_init = z_init
+        ###self.chi_init = chi_init
+        ###self.l_init = l_init
+        ###self.z_init = z_init
 
         # sum numbers for making an initial cut
-        self.chi_abs = chi_abs
-        self.zmin_abs = zmin_abs
-        self.zmax_abs = zmax_abs
-        self. l_abs = l_abs
+        ###self.chi_abs = chi_abs
+        ###self.zmin_abs = zmin_abs
+        ###self.zmax_abs = zmax_abs
+        ###self. l_abs = l_abs
 
         self.zmin = zmin #minimum redshift
         self.zmax = zmax #maximum redshift
@@ -95,6 +94,7 @@ class calibrate(object):
         self.abnod = np.linspace(self.zmin,self.zmax,self.Nab) # the afterburner nods
         self.chinod = np.linspace(self.zmin,self.zmax,self.Nchi) #the chimax nods
 
+        # the inferred c-m relation parameters
         self.theta = np.loadtxt("opt_theta_bfgs_bounded2.txt")
         self.m = self.theta[0:3*(self.Nm-1)].reshape(self.Nm-1,3) #array of m-nodes
         self.b = self.theta[3*(self.Nm-1):3*(self.Nm+self.Nb-2)].reshape(self.Nb-1,3) #array of b-nodes
@@ -115,14 +115,13 @@ class calibrate(object):
         znods = np.linspace(self.zmin, self.zmax, 36)
 	znods = .5*(znods[1:]+znods[:-1])
 	#####################################HACKY#############################
-        znods = znods[znods<0.7]
         mrefs = mrefs[znods<0.7]
+        znods = znods[znods<0.7]
 	#######################################################################
         self.xref = CubicSpline(znods, mrefs)(self.xrefnod)
         
 	#######################
-        self.dz_theta_0 = dz_theta_0
-        self.chimax_theta_0 = chimax_theta_0 
+        self.dz_theta_zero = 0.01
 
         return None
 
@@ -186,7 +185,7 @@ class calibrate(object):
     def solve_ab_lnlike(self, mask):
         """
 	solves ab_lnlike
-	dz_theta_0 = initial guess for dz_theta
+	dz_theta_zero = initial guess for dz_theta
 	mask = some mask on chi and l of spec red gals
         """
         nll = lambda *args: self.ab_lnlike(*args)
@@ -194,66 +193,31 @@ class calibrate(object):
 	
 	return result["x"]
     
-    def chimax_lnlike(self, chimax_theta):
-        """
-        temp estimate of chimax_theta at the loc of chi nods
-	"""
-
-	#first have to run the ab
-        #alt chinods , norm =  np.exp(chimax_theta[:-1]), np.exp(chimax_theta[-1]) 
-        chinods =  np.exp(chimax_theta)
-	chi_maxes = CubicSpline(nods ,chinods)(self.calib[:,1])
-	mask = (self.calib[:,2] < chimax_calib)&(self.calib[:,3]>self.lmin)
-        dz_ab =  solve_ab_lnlike(self, mask)
-	print "CURRENT ESTIMATE OF AB = " , dz_a
-
-	# calibrate the calib-zs and rezs
-	self.calib[:,1] = self.calib[:,1] - CubicSpline(self.abnod, dz_ab)(self.z) #calib gals
-	self.z = self.z - CubicSpline(self.abnod , dz_ab)(self.z) #all gals
-        #calibrate the red-chis and red-ls
-        self.chi = self.chi3d(z)
-	self.l = self.l3d(z)
-        #mask the red-ls that are larger than self.lmin (=0.5 or 1)
-	mask = self.l > self.lmin
-      
-    	#chinods , norm = np.exp(chimax_theta[:-1]), np.exp(chimax_theta[-1])  
-    	chinods = np.exp(chimax_theta)
-    	
-	chi_maxes = CubicSpline(nods ,chinods)(self.z[mask])
-    	sample_z = self.z[self.chi < chi_maxes]
-    	hist , edges = np.histogram(sample_z, bins = self.Nhist)
-    	bins = .5*(edges[1:]+edges[:-1])
-    	dbin = edges[1] - edges[0]
-    	dvdbin = dvdz(bins)
-    	dvbin = dvdbin * dbin
-    	chisq = np.sum((hist - self.norm*dvbin)**2./(hist+ self.norm*dvbin)) 
-        
-	return chisq
  
-    def chimax_lnlike(chimax_theta):
+    def chimax_lnlike(self, chimax_theta):
 
 	#first have to run the ab
         #alt chinods , norm =  np.exp(chimax_theta[:-1]), np.exp(chimax_theta[-1]) 
         chinods =  np.exp(chimax_theta)
-	chi_max_calib = CubicSpline(self.chinods ,chinods)(self.calib[:,1])
+        chimax_calib = CubicSpline(self.chinod ,chinods)(self.calib[:,1])
 	mask = (self.calib[:,2] < chimax_calib)&(self.calib[:,3] > self.lmin)
-        dz_ab = solve_ab_lnlike(self, mask)
+        dz_ab = self.solve_ab_lnlike(mask)
 	print "CURRENT ESTIMATE OF AB = " , dz_ab
-
-	# calibrate the calib-zs and rezs
-	self.calib[:,1] = self.calib[:,1] - CubicSpline(self.abnod, dz_ab)(self.calib[:,1]) #calib gals
+        # calibrate the calib-zs and rezs
+        self.calib[:,1] = self.calib[:,1] - CubicSpline(self.abnod, dz_ab)(self.calib[:,1]) #calib gals
 	self.z = self.z - CubicSpline(self.abnod , dz_ab)(self.z) #all gals
         #calibrate the red-chis and red-ls
         self.chi = self.chi3d()
+	##print "DONE WITH THE HARD"
 	self.l = self.l3d()
         #mask the red-ls that are larger than self.lmin (=0.5 or 1)
-	mask = self.l > self.lmin
+        mask = self.l > self.lmin
       
     	#chinods , norm = np.exp(chimax_theta[:-1]), np.exp(chimax_theta[-1])  
     	chinods = np.exp(chimax_theta)
     	norm = self.nbar * 360.3 / (41252.96)
     	
-	chi_maxes = CubicSpline(self.chinods ,chinods)(self.z)
+	chi_maxes = CubicSpline(self.chinod ,chinods)(self.z)
     	sample3 = self.z[self.catalog[:,2]<chi_maxes]
 	hist , edges = np.histogram(sample3[:,1], bins = self.Nhist)
     	bins = .5*(edges[1:]+edges[:-1])
@@ -271,15 +235,14 @@ class calibrate(object):
 	solves chi_max_lnlike
         """
 
-    	chinods_0 = 2 + np.zeros((self.Nhist))
-    	nll = lambda *args: chimax_lnlike(*args)
-           bnds.append((np.log(1.),np.log(3)))
-       	print norm_bound
-       	bnds.append((-10., -1.))
-    	result = op.minimize(nll, np.log(chinods_0), args=sample2, method='SLSQP', bounds = bnds, options={'disp': True ,'eps' : .001})
-    	print "processed fraction" , processed.shape[0] / 34290660.0
-    	print "result" , np.exp(result["x"])
-    	density = np.exp(result["x"])[-1] * processed.shape[0]* 360.3 / (41253 * 34290660)  
+    	chinods_0 = 2 + np.zeros((self.Nchi))
+        print "chinods_0" , chinods_0    	
+	nll = lambda *args: self.chimax_lnlike(*args)
+        bnds = []
+	for h in range(len(chinods_0)):
+	    bnds.append((np.log(1.),np.log(3)))
+    	result = op.minimize(nll, np.log(chinods_0) , method='SLSQP', bounds = bnds, options={'disp': True ,'eps' : .001})
+    	print "esimated chis" , np.exp(result["x"])
 
         return None
          
@@ -317,7 +280,7 @@ if __name__ == '__main__':
    arg_one = np.argsort(ID[mask_one])
    arg_two = np.argsort(spec_ID[mask_two])
 
-   print (ID[mask_one][arg_one] == spec_ID[mask_two][arg_two]) #sanity check!
+   ##print (ID[mask_one][arg_one] == spec_ID[mask_two][arg_two]) #sanity check!
 
    spec_red_sample = red_sample[mask_one][arg_one]
    mask_chil = (spec_red_sample[:,0]==1)
@@ -325,13 +288,29 @@ if __name__ == '__main__':
    
    calib_sample = np.vstack([spec_red_sample.T , spec_specz]).T
    
-   print calib_sample #sanity check
+   ##print calib_sample #sanity check
    
 
    ########## INITIALIZATION OF THE EZGAL MODEL ########
 
-   model = ezgal.model("/net/delft/data2/vakili/easy/ezgal_models/www.baryons.org/ezgal/models/bc03_burst_0.1_z_0.02_salp.model")
+   model = ezgal.model("/net/vuntus/data2/vakili/easy/ezgal_models/www.baryons.org/ezgal/models/bc03_burst_0.1_z_0.02_salp.model")
    zf = 3.0 #HARDCODED
-   model.add_filter("/net/delft/data2/vakili/easy/i.dat" , "kids" , units = "nm")
+   model.add_filter("/net/vuntus/data2/vakili/easy/i.dat" , "kids" , units = "nm")
    model.set_normalization("sloan_i" , 0.2 , 17.85, vega=False, apparent=True)
    cosmo = {'omega_M_0':0.3, 'omega_lambda_0':0.7, 'omega_k_0':0.0, 'h':1.0}
+
+   ######## calibration ##########
+
+   zmin, zmax, dz = 0.1, 0.8, 0.02
+   Nb, Nm, Nf = 15, 8, 6
+   Nhis, Nab, Nchi = 10, 7, 6   
+   lmin, nbar = 0.5, 0.001
+   z, l, chi = red_sample[:,1], red_sample[:,2], red_sample[:,2]
+   calib = calib_sample
+   colors = colors
+   color_errs = color_errs
+   mis = mi
+
+   cal = calibrate(zmin, zmax, dz, Nb, Nm, Nf, Nhis, Nab, Nchi , lmin, nbar, z, l, chi, colors, color_errs, mis, calib)
+
+   cal.solve_chimax_lnlike()
